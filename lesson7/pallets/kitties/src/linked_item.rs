860f06e978bfa8fa8c884313d288c7c23054d2b0
jasonruan <rzexin@qq.com>
2020-06-29 18:39:22 +0800

    代码格式化。

--- lesson7/pallets/kitties/src/linked_item.rs
+++ lesson7/pallets/kitties/src/linked_item.rs
@@ -1,45 +1,46 @@
-use frame_support::{StorageMap, Parameter};
+use codec::{Decode, Encode};
+use frame_support::{Parameter, StorageMap};
 use sp_runtime::traits::Member;
-use codec::{Encode, Decode};
 
 #[cfg_attr(feature = "std", derive(Debug, PartialEq, Eq))]
 #[derive(Encode, Decode)]
 pub struct LinkedItem<Value> {
-	pub prev: Option<Value>,
-	pub next: Option<Value>,
+    pub prev: Option<Value>,
+    pub next: Option<Value>,
 }
 
 pub struct LinkedList<Storage, Key, Value>(sp_std::marker::PhantomData<(Storage, Key, Value)>);
 
-impl<Storage, Key, Value> LinkedList<Storage, Key, Value> where
-	Value: Parameter + Member + Copy,
-	Key: Parameter,
-	Storage: StorageMap<(Key, Option<Value>), LinkedItem<Value>, Query = Option<LinkedItem<Value>>>,
+impl<Storage, Key, Value> LinkedList<Storage, Key, Value>
+where
+    Value: Parameter + Member + Copy,
+    Key: Parameter,
+    Storage: StorageMap<(Key, Option<Value>), LinkedItem<Value>, Query = Option<LinkedItem<Value>>>,
 {
-	fn read_head(key: &Key) -> LinkedItem<Value> {
-		Self::read(key, None)
-	}
+    fn read_head(key: &Key) -> LinkedItem<Value> {
+        Self::read(key, None)
+    }
 
-	fn write_head(account: &Key, item: LinkedItem<Value>) {
-		Self::write(account, None, item);
-	}
+    fn write_head(account: &Key, item: LinkedItem<Value>) {
+        Self::write(account, None, item);
+    }
 
-	fn read(key: &Key, value: Option<Value>) -> LinkedItem<Value> {
-		Storage::get((&key, value)).unwrap_or_else(|| LinkedItem {
-			prev: None,
-			next: None,
-		})
-	}
+    fn read(key: &Key, value: Option<Value>) -> LinkedItem<Value> {
+        Storage::get((&key, value)).unwrap_or_else(|| LinkedItem {
+            prev: None,
+            next: None,
+        })
+    }
 
-	fn write(key: &Key, value: Option<Value>, item: LinkedItem<Value>) {
-		Storage::insert((&key, value), item);
-	}
+    fn write(key: &Key, value: Option<Value>, item: LinkedItem<Value>) {
+        Storage::insert((&key, value), item);
+    }
 
-	pub fn append(key: &Key, value: Value) {
+    pub fn append(key: &Key, value: Value) {
         // 作业
-	}
+    }
 
-	pub fn remove(key: &Key, value: Value) {
+    pub fn remove(key: &Key, value: Value) {
         // 作业
-	}
- }
+    }
+}
