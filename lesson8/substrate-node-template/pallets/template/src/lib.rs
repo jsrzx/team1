c5f102ab19a73f718e196948e7a2a942917f414b
jasonruan <rzexin@qq.com>
2020-07-06 18:38:13 +0800

    代码格式化

--- lesson8/substrate-node-template/pallets/template/src/lib.rs
+++ lesson8/substrate-node-template/pallets/template/src/lib.rs
@@ -20,77 +20,77 @@ mod tests;
 
 /// The pallet's configuration trait.
 pub trait Trait: system::Trait {
-	// Add other types and constants required to configure this pallet.
+    // Add other types and constants required to configure this pallet.
 
-	/// The overarching event type.
-	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+    /// The overarching event type.
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 // This pallet's storage items.
 decl_storage! {
-	// It is important to update your storage name so that your pallet's
-	// storage items are isolated from other pallets.
-	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Just a dummy storage item.
-		// Here we are declaring a StorageValue, `Something` as a Option<u32>
-		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
-		Something get(fn something): Option<u32>;
-	}
+    // It is important to update your storage name so that your pallet's
+    // storage items are isolated from other pallets.
+    // ---------------------------------vvvvvvvvvvvvvv
+    trait Store for Module<T: Trait> as TemplateModule {
+        // Just a dummy storage item.
+        // Here we are declaring a StorageValue, `Something` as a Option<u32>
+        // `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
+        Something get(fn something): Option<u32>;
+    }
 }
 
 // The pallet's events
 decl_event!(
-	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
-	}
+    pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+        /// Just a dummy event.
+        /// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
+        /// To emit this event, we call the deposit function, from our runtime functions
+        SomethingStored(u32, AccountId),
+    }
 );
 
 // The pallet's errors
 decl_error! {
-	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
-	}
+    pub enum Error for Module<T: Trait> {
+        /// Value was None
+        NoneValue,
+        /// Value reached maximum and cannot be incremented further
+        StorageOverflow,
+    }
 }
 
 // The pallet's dispatchable functions.
 decl_module! {
-	/// The module declaration.
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Initializing errors
-		// this includes information about your errors in the node's metadata.
-		// it is needed only if you are using errors in your pallet
-		type Error = Error<T>;
+    /// The module declaration.
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // Initializing errors
+        // this includes information about your errors in the node's metadata.
+        // it is needed only if you are using errors in your pallet
+        type Error = Error<T>;
 
-		// Initializing events
-		// this is needed only if you are using events in your pallet
-		fn deposit_event() = default;
+        // Initializing events
+        // this is needed only if you are using events in your pallet
+        fn deposit_event() = default;
 
-		#[weight = 10_000]
-		pub fn save_number(origin, number: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
+        #[weight = 10_000]
+        pub fn save_number(origin, number: u32) -> dispatch::DispatchResult {
+            // Check it was signed and get the signer. See also: ensure_root and ensure_none
+            let who = ensure_signed(origin)?;
 
-			/*******
-			 * 学员们在这里追加逻辑
-			 *******/
+            /*******
+             * 学员们在这里追加逻辑
+             *******/
 
-			Ok(())
-		}
+            Ok(())
+        }
 
-		fn offchain_worker(block_number: T::BlockNumber) {
-			debug::info!("Entering off-chain workers");
+        fn offchain_worker(block_number: T::BlockNumber) {
+            debug::info!("Entering off-chain workers");
 
-			/*******
-			 * 学员们在这里追加逻辑
-			 *******/
-		}
+            /*******
+             * 学员们在这里追加逻辑
+             *******/
+        }
 
-	}
+    }
 }
